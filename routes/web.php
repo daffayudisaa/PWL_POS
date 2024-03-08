d297c435c880e99167b53dd7a8eec4dbc91d4a55
daffayudisaa <daffayudisa09@gmail.com>
2024-03-08 22:14:12 +0700

    feat: Praktikum 5 (Implementasi Query Builder)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
 
 
 /*
@@ -20,3 +21,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
