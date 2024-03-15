b1d213be3f514bea6c61d4065fab8c36f0fb4882
daffayudisaa <daffayudisa09@gmail.com>
2024-03-15 14:09:43 +0700

    feat: Praktikum 2.3 (Retreiving Aggregrates)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,8 +11,11 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)-> count();
+        //dd($user);
         return view('user', ['data' => $user]);
+
+        //$user = UserModel::where('username', 'manager9')->firstOrFail();
 
         //$user = UserModel::findOrFail(1);
 
