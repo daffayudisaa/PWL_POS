64ba5f851fea36a6e2be6bbacad9d80b8b9145aa
daffayudisaa <daffayudisa09@gmail.com>
2024-03-21 20:53:46 +0700

    feat: Praktikum 3 (Membuat form kemudian menyimpan data dalam database)

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -5,12 +5,29 @@ namespace App\Http\Controllers;
 use App\DataTables\KategoriDataTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\KategoriModel;
 
 class KategoriController extends Controller
 {
     public function index(KategoriDataTable $dataTable)
     {
         return $dataTable->render('kategori.index');
+
+    }
+
+    public function create()
+    {
+        return view('kategori.create');
+    }
+
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori,
+        ]);
+        return redirect('/kategori');
+    }
 
         // $data=[
         //     'kategori_kode' => 'SNK',
@@ -28,5 +45,5 @@ class KategoriController extends Controller
 
         // $data = DB::table('m_kategori')->get();
         // return view('kategori', ['data' => $data]);
-    }
+    
 }
